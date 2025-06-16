00644743489fd20b64ae77f9df3e4d4361624032
Elfld <enprondena@gmail.com>
2025-06-16 23:41:04 +0900

    Fix: api rewrites for vercel - https > http

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,7 +7,6 @@ const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({ baseDirectory: __dirname });
 
-// 아래처럼 config() 형태로 변경
 const eslintConfig = [
   ...compat.config({
     extends: ["next/core-web-vitals"],
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: process.env.API_URL + '/:path*',  // 예: 'http://your-http-api.com'
+      },
+    ];
+  },
+};
 
 export default nextConfig;
